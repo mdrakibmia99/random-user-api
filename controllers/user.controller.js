be4b47c2d5657046fa52be02355b0104ac6f962d
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-09-04 21:25:07 +0600

    create  for fet a random user data

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,3 +5,13 @@ const randomID = require("../utils/randomID");
 // internal import
 const users = require(__dirname + "/../public/user.json");
 
+// Get a random user data
+module.exports.getARandomUser = (req, res) => {
+    const rand = randomID(users.length);
+
+    res.status(200).json({
+        success: true,
+        message: "successfully fetching a random user data",
+        data: users.find(user => user.id === rand)
+    });
+}
