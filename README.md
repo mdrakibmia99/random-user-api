cdcb15497aab8bc61a6729503fd0873933ab3e70
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-09-04 21:17:09 +0600

    app.js file and public folder create

---
+++ README.md
@@ -0,0 +1,2 @@
+# Random User API
+A rest API that is created and store in local `.json` file format. It convey through URL and interact as a direct backend.
---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Title: Driver code
+ * Description: Consume all operations & operands
+ * Author: Md Rakib Mia
+ */
+
+// external imports
+const express = require("express");
+const cors = require("cors");
+
+
+// internal imports
+const app = express();
+const port = process.env.PORT || 5000;
+
+// connect with middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/user", userRouter);
+
+// enable requests
+app.get("/", (req, res) => {
+    res.status(200).sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
